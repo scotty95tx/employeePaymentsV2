dfc705dce26b35c677ce594769772a4d39ec7194
Scott Lowe <Scott.Lowe@hcss.com>
2024-01-10 17:39:01 -0600

    Restructured code. Now instead of creating a new calculator class and extending off and creating a calculator class for every employee type, we create a class for the employee itself, and allow each employee to have their own calculator based off the type of employee they are.

--- calculatePayments.js
+++ calculatePayments.js
@@ -1,118 +1,106 @@
-class PaymentCalculator {
-    calculateEmployeePayment(id, employeeData, payrollExport) {
-        this.employeeData = employeeData
-        this.id = employeeData[id].id
-        this.name = employeeData[id].name
-        this.hourlyRate = employeeData[id].hourlyRate
-        this.type = employeeData[id].type
-        
-        for (let i = 0; i < payrollExport.length; i++) {
-            if (payrollExport[i].id === this.id) {
-                this.hoursWorked = payrollExport[i].hours
-            }
-        }
-        
-        this.totalPay = this.hourlyRate * this.hoursWorked
-
-        for (let i = 0; i < payrollExport.length; i++) {
-            if (payrollExport[i].id === this.id) {
-                this.hoursWorked = payrollExport[i].hours
-            }
-        }
+class Employee {
+    constructor(id, name, hourlyRate) {
+      this.id = id;
+      this.name = name;
+      this.hourlyRate = hourlyRate;
     }
-}
-
-class CalculateLaborerPay extends PaymentCalculator {
-    calculateEmployeePayment(id, employeeData, payrollExport) {
-        super.calculateEmployeePayment(id, employeeData, payrollExport)
-        if (this.hoursWorked > 10) {
-            this.totalPay += this.hoursWorked * 1 + 20
+    getSalary(payrollExport) {
+      let hoursWorked = 0;
+      let totalPay = 0;
+  
+      for (let i = 0; i < payrollExport.length; i++) {
+        if (payrollExport[i].id === this.id) {
+          hoursWorked += payrollExport[i].hours;
         }
-        
-        return {hours: this.hoursWorked, name: this.name, type: this.type, pay: this.totalPay}
+      }
+  
+      totalPay += this.hourlyRate * hoursWorked;
+  
+      return {
+        hours: hoursWorked,
+        name: this.name,
+        type: "None",
+        pay: totalPay,
+      };
     }
-}
-
-class CalculateForemanPay extends PaymentCalculator {
-    calculateEmployeePayment(id, employeeData, payrollExport) {
-        super.calculateEmployeePayment(id, employeeData, payrollExport)
-        if (this.hoursWorked > 15) {
-            this.totalPay += this.hourlyRate * .05 * this.hoursWorked
-        }
-
-        return {hours: this.hoursWorked, name: this.name, type: this.type, pay: this.totalPay}
+  }
+  
+  class Laborer extends Employee {
+    constructor(id, name, hourlyRate) {
+      super(id, name, hourlyRate);
     }
-}
-
-class CalculateSuperPay extends PaymentCalculator {
-    calculateEmployeePayment(id, employeeData, payrollExport) {
-        super.calculateEmployeePayment(id, employeeData, payrollExport)
-        if (this.hoursWorked > 20) {
-            this.totalPay += 50
-        }
-        return {hours: this.hoursWorked, name: this.name, type: this.type, pay: this.totalPay}
+  
+    getSalary(payrollExport) {
+      let hoursWorked = super.getSalary(payrollExport).hours;
+      let totalPay = super.getSalary(payrollExport).pay;
+  
+      if (hoursWorked > 10) {
+        totalPay += hoursWorked * 1 + 20;
+      }
+  
+      return {
+        hours: hoursWorked,
+        name: this.name,
+        type: "Laborer",
+        pay: totalPay,
+      };
     }
-}
-
-const employeeData = {
-    1079 : {
-        id: 1079,
-        name: "Bob",
-        age: 29,
-        gender: "male",
-        race: "Native Hawaiian",
-        dateOfBirth: "Feb 14th, 1994",
-        hourlyRate: 45,
-        type: "Laborer"
-    },
-    4020 : {
-        id: 4020,
-        name: "Sir William Wallace",
-        age: 753,
-        gender: "male",
-        race: "Scottish",
-        dateOfBirth: "Dec 12th, 1270",
-        hourlyRate: 50,
-        type: "Foreman"
-    },
-    123 : {
-        id: 123,
-        name: "Thomas Shelby",
-        age: 133,
-        gender: "male",
-        race: "Irish",
-        dateOfBirth: "June 6th, 1890",
-        hourlyRate: 80,
-        type: "SuperIntendent"
-    },
-    8759 : {
-        id: 8759,
-        name: "Ragnar Lothbrok",
-        age: 1268,
-        gender: "male",
-        race: "Danish",
-        dateOfBirth: "August 30th, 755",
-        hourlyRate: 30,
-        type: "Laborer"
+  }
+  
+  class Foreman extends Employee {
+    constructor(id, name, hourlyRate) {
+      super(id, name, hourlyRate);
     }
-}
-
-const payrollExport = [{id: 8759, hours: 50}, {id: 4020, hours: 55}, {id: 1079, hours: 45}, {id: 123, hours: 52}]
-
-function calculateAllPay(employeeData, payrollExport) {
-    let allPay = [{SUCCESS: true, ERROR: 'none'}]
-    for (let i = 0; i < payrollExport.length; i++) {
-        if (employeeData[payrollExport[i].id].type == "Laborer") {
-            allPay.push(new CalculateLaborerPay().calculateEmployeePayment(payrollExport[i].id, employeeData, payrollExport))
-        } else if (employeeData[payrollExport[i].id].type == "Foreman") {
-            allPay.push(new CalculateForemanPay().calculateEmployeePayment(payrollExport[i].id, employeeData, payrollExport))
-        } else if (employeeData[payrollExport[i].id].type == "SuperIntendent") {
-            allPay.push(new CalculateSuperPay().calculateEmployeePayment(payrollExport[i].id, employeeData, payrollExport))
-        } else {
-            return [{SUCCESS: false, ERROR: `Employeee ${payrollExport[i].id} does not have a valid type`}]
-        }
+    getSalary(payrollExport) {
+      let hoursWorked = super.getSalary(payrollExport).hours;
+      let totalPay = super.getSalary(payrollExport).pay;
+  
+      if (hoursWorked > 15) {
+        totalPay += this.hourlyRate * 0.05 * hoursWorked;
+      }
+  
+      return {
+        hours: hoursWorked,
+        name: this.name,
+        type: "Foreman",
+        pay: totalPay,
+      };
     }
-    return allPay
-}
-
-console.log(calculateAllPay(employeeData, payrollExport))
+  }
+  
+  class Super extends Employee {
+    constructor(id, name, hourlyRate) {
+      super(id, name, hourlyRate);
+    }
+    getSalary(payrollExport) {
+      let hoursWorked = super.getSalary(payrollExport).hours;
+      let totalPay = super.getSalary(payrollExport).pay;
+  
+      if (hoursWorked > 20) {
+        totalPay += 50;
+      }
+      return {
+        hours: hoursWorked,
+        name: this.name,
+        type: "Super",
+        pay: totalPay,
+      };
+    }
+  }
+  
+  let employees = [
+    new Laborer(8579, "bob", 25),
+    new Foreman(100, "Pete", 40),
+    new Super(123, "Daniel", 18),
+  ];
+  
+  const payrollExport = [
+    {id: 8579, hours: 50},
+    {id: 4020, hours: 55},
+    {id: 100, hours: 45},
+    {id: 123, hours: 52},
+  ];
+  
+  for (let i = 0; i < employees.length; i++) {
+    console.log(employees[i].getSalary(payrollExport));
+  }
